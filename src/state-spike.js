b1b98e3ea5b08594f485af3f282bc72c96671cd3
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-09-04 16:09:31 +1000

    example state

---
+++ src/state-spike.js
@@ -0,0 +1,23 @@
+const exampleState = {
+    inventory: [
+        {
+            type: "note",
+            text: "lorem ipsum",
+            icon: "some icon image imported"
+        },
+        {
+            type: "image",
+            image: "some imported image",
+            icon: "some icon image imported"
+        },
+        {
+            type: "object",
+            description: "a knife",
+            icon: "some icon image imported"
+        }
+    ],
+    timelinePosition: {
+        position: "house",
+        scene: "artroom"
+    }
+}

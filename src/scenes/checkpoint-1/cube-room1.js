7640b6a3dd9148a760e31e4ef32232cfe7464a71
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-09-05 18:04:09 +1000

    cube dimension obj exists w protections across time

--- src/scenes/checkpoint-1/cube-room1.js
+++ src/scenes/checkpoint-1/cube-room1.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { updateText } from '../../dom/text-update';
 import { getInteractionManager } from '../../setup';
+import { addImageToInventory, addNoteToInventory, addObjectToInventory, isImageInInventory, isNoteInInventory, isObjectInInventory } from '../../state-management/inventory-state';
+import { checkTimeLine } from '../../state-management/timeline-state';
 
 export function cubeRoom1Factory() {
     const scene = new THREE.Scene();
@@ -9,9 +11,18 @@ export function cubeRoom1Factory() {
 
     // call evilCube giving it scene and interaction manager and assign that to a variable
     const clickableEvilCube = evilCube(scene, interactionManager);
+    const inventoryRandomObject = randomObj(scene, interactionManager);
 
     clickableEvilCube.addEventListener('click', function (e) {
         // changeRoom(cubeRoom1Factory);
+    });
+
+    inventoryRandomObject.addEventListener('click', function (e) {
+        if (checkTimeLine("house-1")) {
+            if (!isObjectInInventory("cube dimensh obj")) {
+            addObjectToInventory("cube dimensh obj", "none");
+            };
+        }
     });
     
     updateText("Escape is no longer an option");
@@ -30,4 +41,17 @@ function evilCube(scene, interactionManager) {
     interactionManager.add(evilCube);
 
     return evilCube;
-}
+};
+
+function randomObj(scene, interactionManager) {
+    const geometry = new THREE.BoxGeometry(0.6, 0.6, 0.6);
+    const material = new THREE.MeshBasicMaterial({wireframe: true, color: "yellow"});
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = 3;
+    cube.position.y = -2;
+    cube.position.z = 3;
+
+    scene.add(cube);
+    interactionManager.add(cube);
+    return cube;
+};

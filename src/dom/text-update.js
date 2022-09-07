709c055f735c20979080e9765a551b9cde62c937
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-09-07 21:30:43 +1000

    removed comments

--- src/dom/text-update.js
+++ src/dom/text-update.js
@@ -2,11 +2,11 @@ const textDiv = document.getElementById('text-display');
 
 export function hideText() {
     textDiv.classList.add('hidden');
-}
+};
 
 export function unhideText() {
     textDiv.classList.remove('hidden');
-}
+};
 
 export function updateText(str) {
 
@@ -20,16 +20,11 @@ export function updateText(str) {
 
     clearText();
     replaceText(str);
-}
-
-// TODO autosave text update function
-
-// TODO call it from the relevent places i.e. when changing timeline stuff so loadRoom changeCheckpoint should trigger autosave
+};
 
 export function autoSavetext() {
     const autoSaveText = document.getElementById('autosave');
     autoSaveText.innerText = "Auto Save ...";
-    // use a settimeout to have it deleted after like 10 seconds or like two or something
     setTimeout(clearAutoSaveText, 3500);
 
     function clearAutoSaveText() {
